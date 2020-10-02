ec27972808c642f6fbd6c17d38aeab26e3c439b3
Franz Andel <franz.andel@amarbank.co.id>
2020-10-02 13:40:00 +0700

    feat(payment-success): Handle Payment Success Page

---
+++ app/src/main/java/com/franzandel/selleverything/paymentsuccess/PaymentSuccessActivity.kt
@@ -0,0 +1,27 @@
+package com.franzandel.selleverything.paymentsuccess
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.franzandel.selleverything.HomeActivity
+import com.franzandel.selleverything.R
+import com.franzandel.selleverything.extension.goTo
+
+class PaymentSuccessActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_success)
+
+        closeActivityAfter2Seconds()
+    }
+
+    private fun closeActivityAfter2Seconds() {
+        Handler().postDelayed({
+            kotlin.run {
+                goTo(HomeActivity::class.java)
+                finishAffinity()
+            }
+        }, 2000L)
+    }
+}
